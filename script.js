767707fd666b394cdb3cfe5d7925b755ee39addc
Runar <runar.hoydal@lyse.net>
2022-10-20 14:44:24 +0200

    Can now write, edit and delete multiple entries to a table
    table has been moved to the same page rather than a separate page

--- script.js
+++ script.js
@@ -16,17 +16,75 @@ document.addEventListener("click", e => {
 
 
 
-const resultsList = document.getElementById("results");
-var table = document.getElementById("Timeliste-log")
-var row = table.insertRow(-1)
+var rad = null;
+function SendInn() {
+    var dataLagtTil = hentData();
+    var lesData = hentDataFraLocalStorage(dataLagtTil);
+    if(rad==null){
+        leggTilData(lesData);
+    }
+    else{
+        oppdatere();
+    }
+}
 
-var count = 0
-new URLSearchParams(window.location.search).forEach((value, name) => {
-    var currentCell = row.insertCell(count)
-    currentCell.innerHTML = value
-    count++
-})
+function hentData(){
+    var dato = document.getElementById("dato").value;
+    var start = document.getElementById("start").value;
+    var slutt = document.getElementById("slutt").value;
+    var beskrivelse = document.getElementById("beskrivelse").value;
 
+
+    return [dato, start, slutt, beskrivelse]
+}
+
+function hentDataFraLocalStorage(dataLagtTil) {
+    var DatoLagret = localStorage.setItem("Dato", dataLagtTil[0]);
+    var StartLagret = localStorage.setItem("Start", dataLagtTil[1]);
+    var SluttLagret = localStorage.setItem("Slutt", dataLagtTil[2]);
+    var BeskrivelseLagret = localStorage.setItem("Beskrivelse", dataLagtTil[3]);
+
+    var DatoHentet = localStorage.getItem("Dato", DatoLagret);
+    var StartHentet = localStorage.getItem("Start", StartLagret);
+    var SluttHentet = localStorage.getItem("Slutt", SluttLagret);
+    var BeskrivelseHentet = localStorage.getItem("Beskrivelse", BeskrivelseLagret);
+
+    return [DatoHentet, StartHentet, SluttHentet, BeskrivelseHentet];
+}
+
+
+function leggTilData(lesData) {
+    var table = document.getElementById("Timeliste-log");
+    var rad = table.insertRow();
+    rad.insertCell(0).innerHTML = lesData[0];
+    rad.insertCell(1).innerHTML = lesData[1];
+    rad.insertCell(2).innerHTML = lesData[2];
+    rad.insertCell(3).innerHTML = lesData[3];
+    rad.insertCell(4).innerHTML = `
+    <button onclick = endre(this)>Endre</button>
+    <button onclick = slett(this)>Slett</button>`;
+}
+
+function endre(td) {
+    rad = td.parentElement.parentElement;
+    document.getElementById("dato").value = rad.cells[0].innerHTML;
+    document.getElementById("start").value = rad.cells[1].innerHTML;
+    document.getElementById("slutt").value = rad.cells[2].innerHTML;
+    document.getElementById("beskrivelse").value = rad.cells[3].innerHTML;
+}
+
+function oppdatere(){
+    rad.cells[0].innerHTML = document.getElementById("dato").value;
+    rad.cells[1].innerHTML = document.getElementById("start").value;
+    rad.cells[2].innerHTML = document.getElementById("slutt").value;
+    rad.cells[3].innerHTML = document.getElementById("beskrivelse").value;
+    rad = null;
+}
+
+function slett(td){
+    rad = td.parentElement.parentElement;
+    document.getElementById("Timeliste-log").deleteRow(rad.rowIndex);
+}
 
 
 //const resultsList = document.getElementById("Timeføring").onsubmit();
@@ -42,3 +100,14 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
 
 
 
+
+//const resultsList = document.getElementById("results");
+//var table = document.getElementById("Timeliste-log");
+//var row = table.insertRow(-1);
+
+//var count = 0
+//new URLSearchParams(window.location.search).forEach((value) => {
+//    var currentCell = row.insertCell(count)
+//    currentCell.innerHTML = value
+//    count++
+//}) 
