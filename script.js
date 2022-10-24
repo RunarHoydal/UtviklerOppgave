4ba8993af0846c151d51769e1c0378962b612eca
Runar <runar.hoydal@lyse.net>
2022-10-24 12:16:16 +0200

    Can now update and delete table entries
    some styling has also been done to the form and the table

--- script.js
+++ script.js
@@ -20,17 +20,22 @@ document.addEventListener("click", e => {
 
 // Storing data entries
 var arr = new Array();
-
+var row = null;
 function addData(){
-	getData();
-	
-	arr.push({
-		date: document.getElementById("date").value,
-		start: document.getElementById("start").value,
-		end: document.getElementById("end").value,
-		description: document.getElementById("description").value,
-	});
-	localStorage.setItem("localData", JSON.stringify(arr));
+	if (row == null){
+		getData();
+		
+		arr.push({
+			date: document.getElementById("date").value,
+			start: document.getElementById("start").value,
+			end: document.getElementById("end").value,
+			description: document.getElementById("description").value,
+		});
+		localStorage.setItem("localData", JSON.stringify(arr));
+	}
+	else{
+		updateData();
+	}
 	showData();
 }
 
@@ -41,9 +46,9 @@ function getData(){
 		arr = JSON.parse(str);
 }
 
-function deleteData(){
-	localStorage.clear()
-}
+//function deleteData(){
+//	localStorage.clear()
+//}
 
 function showData(){
 	getData();
@@ -62,8 +67,59 @@ function showData(){
 		row.insertCell(1).innerHTML = arr[i].start;
 		row.insertCell(2).innerHTML = arr[i].end;
 		row.insertCell(3).innerHTML = arr[i].description;
+		row.insertCell(4).innerHTML = `
+		<input 
+			type= "button" 
+			value = "Endre"
+			class = "button edit"
+			onclick = "editData(this);">
+		<input 
+			type= "button" 
+			value = "Slett" 
+			class = "button delete"
+			onclick = "deleteData(this);">`
 	}
 }
+
+function editData(td){
+	row = td.parentElement.parentElement; 
+	
+	document.getElementById("date").value = row.cells[0].innerHTML;
+	document.getElementById("start").value = row.cells[1].innerHTML;
+	document.getElementById("end").value = row.cells[2].innerHTML;
+	document.getElementById("description").value = row.cells[3].innerHTML;
+}
+
+function updateData(){
+	row.cells[0].innerHTML = document.getElementById("date").value;
+	row.cells[1].innerHTML = document.getElementById("start").value;
+	row.cells[2].innerHTML = document.getElementById("end").value;
+	row.cells[3].innerHTML = document.getElementById("description").value;
+	
+	
+	arr.splice(row.rowIndex - 1, 1, {
+		date: document.getElementById("date").value,
+		start: document.getElementById("start").value,
+		end: document.getElementById("end").value,
+		description: document.getElementById("description").value,
+	})
+	localStorage.setItem("localData", JSON.stringify(arr));
+
+	row = null;
+}
+
+function deleteData(td){
+	getData();
+
+	row = td.parentElement.parentElement;
+	arr.splice(row.rowIndex - 1, 1)
+	document.getElementById("working-hours").deleteRow(row.rowIndex);
+	
+	localStorage.setItem("localData", JSON.stringify(arr));
+	
+}
+
+
 
 window.addEventListener('load', () => {
 	showData();
