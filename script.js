f865fc743a047def00e9c6cc6d5432231ab2c140
Runar <runar.hoydal@lyse.net>
2022-10-24 16:07:54 +0200

    trying to reformat for phone view
    doesn't quite work yet but sceleton is there

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ function addData(){
 		updateData();
 	}
 	showData();
+	showDataMinimized();
 }
 
 function getData(){
@@ -46,9 +47,6 @@ function getData(){
 		arr = JSON.parse(str);
 }
 
-//function deleteData(){
-//	localStorage.clear()
-//}
 
 function showData(){
 	getData();
@@ -79,6 +77,33 @@ function showData(){
 			class = "button delete"
 			onclick = "deleteData(this);">`
 	}
+}
+
+function showDataMinimized(){
+	getData();
+	var minimizedTable = document.getElementById("id-entries");
+	minimizedTable.innerHTML = "";
+	for (i=0; i<arr.length; i++){
+		minimizedTable.innerHTML += `
+		<div class = "entries"> 
+			<input type="button" value = ${i + 1} class = "entries button">
+		</div>
+		<div class = "values"> 
+			<h4>Dato</h4>
+			${arr[i].date}
+			
+			<h4>Start</h4>
+			${arr[i].start}
+			
+			<h4>Slutt</h4>
+			${arr[i].end}
+			
+			<h4>Beskrivelse</h4>
+			${arr[i].description}
+		</div>
+		`
+	}
+
 }
 
 function editData(td){
@@ -123,241 +148,11 @@ function deleteData(td){
 
 window.addEventListener('load', () => {
 	showData();
+	showDataMinimized();
 })
 
 
 
-
-/*
-var rad = null;
-var arr = new Array();
-function SendInn() {
-    var dataLagtTil = hentData();
-    var lesData = hentDataFraLocalStorage(dataLagtTil);
-    if(rad==null){
-        leggTilData(lesData);
-    }
-    else{
-        oppdatereData();
-    }
-    document.getElementById("Timeføring").reset();
-}
-
-function hentData(){
-    var dato = document.getElementById("dato").value;
-    var start = document.getElementById("start").value;
-    var slutt = document.getElementById("slutt").value;
-    var beskrivelse = document.getElementById("beskrivelse").value;
-
-
-    return [dato, start, slutt, beskrivelse]
-}
-
-function hentDataFraLocalStorage(dataLagtTil) {
-    var DatoLagret = localStorage.setItem("Dato", dataLagtTil[0]);
-    var StartLagret = localStorage.setItem("Start", dataLagtTil[1]);
-    var SluttLagret = localStorage.setItem("Slutt", dataLagtTil[2]);
-    var BeskrivelseLagret = localStorage.setItem("Beskrivelse", dataLagtTil[3]);
-
-    var DatoHentet = localStorage.getItem("Dato", DatoLagret);
-    var StartHentet = localStorage.getItem("Start", StartLagret);
-    var SluttHentet = localStorage.getItem("Slutt", SluttLagret);
-    var BeskrivelseHentet = localStorage.getItem("Beskrivelse", BeskrivelseLagret);
-
-    return [DatoHentet, StartHentet, SluttHentet, BeskrivelseHentet];
-}
-
-
-function leggTilData(lesData) {
-    var table = document.getElementById("Timeliste-log");
-    var rad = table.insertRow();
-    rad.insertCell(0).innerHTML = lesData[0];
-    rad.insertCell(1).innerHTML = lesData[1];
-    rad.insertCell(2).innerHTML = lesData[2];
-    rad.insertCell(3).innerHTML = lesData[3];
-    rad.insertCell(4).innerHTML = `
-    <button onclick = endreData(this)>Endre</button>
-    <button onclick = slettData(this)>Slett</button>`;
-}
-
-function endreData(td) {
-    rad = td.parentElement.parentElement;
-    document.getElementById("dato").value = rad.cells[0].innerHTML;
-    document.getElementById("start").value = rad.cells[1].innerHTML;
-    document.getElementById("slutt").value = rad.cells[2].innerHTML;
-    document.getElementById("beskrivelse").value = rad.cells[3].innerHTML;
-}
-
-function oppdatereData(){
-    rad.cells[0].innerHTML = document.getElementById("dato").value;
-    rad.cells[1].innerHTML = document.getElementById("start").value;
-    rad.cells[2].innerHTML = document.getElementById("slutt").value;
-    rad.cells[3].innerHTML = document.getElementById("beskrivelse").value;
-    rad = null;
-}
-
-function slettData(td){
-    rad = td.parentElement.parentElement;
-    document.getElementById("Timeliste-log").deleteRow(rad.rowIndex);
-}
-
-
-
-
-
-
-//const resultsList = document.getElementById("Timeføring").onsubmit();
-//console.log(resultsList)
-//document.addEventListener("click", e => {
-//    console.log(document)
-//})
-
-//function myFunction() {
-//    document.getElementById("Timeføring").submit();
-//    console.log(document.getElementById("Timeføring").submit())
-//  }
-
-
-
-
-//const resultsList = document.getElementById("results");
-//var table = document.getElementById("Timeliste-log");
-//var row = table.insertRow(-1);
-
-//var count = 0
-//new URLSearchParams(window.location.search).forEach((value) => {
-//    var currentCell = row.insertCell(count)
-//    currentCell.innerHTML = value
-//    count++
-//}) 
-
-
-
-
-
-
-
-
-
-window.addEventListener('load', () => {
-	todos = JSON.parse(localStorage.getItem('todos')) || [];
-	const nameInput = document.querySelector('#name');
-	const newTodoForm = document.querySelector('#new-todo-form');
-
-	const username = localStorage.getItem('username') || '';
-
-	nameInput.value = username;
-
-	nameInput.addEventListener('change', (e) => {
-		localStorage.setItem('username', e.target.value);
-	})
-
-	newTodoForm.addEventListener('submit', e => {
-		e.preventDefault();
-
-		const todo = {
-			content: e.target.elements.content.value,
-			category: e.target.elements.category.value,
-			done: false,
-			createdAt: new Date().getTime()
-		}
-
-		todos.push(todo);
-
-		localStorage.setItem('todos', JSON.stringify(todos));
-
-		// Reset the form
-		e.target.reset();
-
-		DisplayTodos()
-	})
-
-	DisplayTodos()
-})
-
-function DisplayTodos () {
-	const todoList = document.querySelector('#todo-list');
-	todoList.innerHTML = "";
-
-	todos.forEach(todo => {
-		const todoItem = document.createElement('div');
-		todoItem.classList.add('todo-item');
-
-		const label = document.createElement('label');
-		const input = document.createElement('input');
-		const span = document.createElement('span');
-		const content = document.createElement('div');
-		const actions = document.createElement('div');
-		const edit = document.createElement('button');
-		const deleteButton = document.createElement('button');
-
-		input.type = 'checkbox';
-		input.checked = todo.done;
-		span.classList.add('bubble');
-		if (todo.category == 'personal') {
-			span.classList.add('personal');
-		} else {
-			span.classList.add('business');
-		}
-		content.classList.add('todo-content');
-		actions.classList.add('actions');
-		edit.classList.add('edit');
-		deleteButton.classList.add('delete');
-
-		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
-		edit.innerHTML = 'Edit';
-		deleteButton.innerHTML = 'Delete';
-
-		label.appendChild(input);
-		label.appendChild(span);
-		actions.appendChild(edit);
-		actions.appendChild(deleteButton);
-		todoItem.appendChild(label);
-		todoItem.appendChild(content);
-		todoItem.appendChild(actions);
-
-		todoList.appendChild(todoItem);
-
-		if (todo.done) {
-			todoItem.classList.add('done');
-		}
-		
-		input.addEventListener('change', (e) => {
-			todo.done = e.target.checked;
-			localStorage.setItem('todos', JSON.stringify(todos));
-
-			if (todo.done) {
-				todoItem.classList.add('done');
-			} else {
-				todoItem.classList.remove('done');
-			}
-
-			DisplayTodos()
-
-		})
-
-		edit.addEventListener('click', (e) => {
-			const input = content.querySelector('input');
-			input.removeAttribute('readonly');
-			input.focus();
-			input.addEventListener('blur', (e) => {
-				input.setAttribute('readonly', true);
-				todo.content = e.target.value;
-				localStorage.setItem('todos', JSON.stringify(todos));
-				DisplayTodos()
-
-			})
-		})
-
-		deleteButton.addEventListener('click', (e) => {
-			todos = todos.filter(t => t != todo);
-			localStorage.setItem('todos', JSON.stringify(todos));
-			DisplayTodos()
-		})
-
-	})
-}
-*/
 
 
 
